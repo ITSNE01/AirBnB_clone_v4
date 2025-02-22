2afadc6bde8e8f6a17a6e2092dbc6d1f2a6a0c60
ITSNE01 <rabie.haddar1@gmail.com>
2025-02-22 09:08:46 -0800

    updates exec

--- web_dynamic/101-hbnb.py
+++ web_dynamic/101-hbnb.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-"""
-Flask App that integrates with AirBnB static HTML Template
-"""
+""" Flask App """
 from flask import Flask, render_template, url_for
 from models import storage
 import uuid;
 
-# flask setup
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
 
 
-# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
-    """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
-    """
+    """ Calls .close() """
     storage.close()
 
 
 @app.route('/101-hbnb')
 def hbnb_filters(the_id=None):
-    """
-    handles request to custom template with states, cities & amentities
-    """
+    """ filters requests """
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
     amens = storage.all('Amenity').values()
@@ -42,6 +34,5 @@ def hbnb_filters(the_id=None):
                            users=users)
 
 if __name__ == "__main__":
-    """
-    MAIN Flask App"""
+    """ run flask app """
     app.run(host=host, port=port)
--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -4,14 +4,13 @@ from flask import Flask, render_template, url_for
 from models import storage
 import uuid;
 
-# flask setup
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
 
 
-# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
     """ Calls .close() """
